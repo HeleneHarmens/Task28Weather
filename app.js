aebe1b2aa9101e2b03bc43c3ee4382586d393804
LineSaevold <linesaevold@hotmail.com>
2019-03-26 10:34:07 +0100

    Kart

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const express = require('express');
-var app = express();
-var path = require('path');
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'))
+const express = require("express");
+var app = express();
+var path = require("path");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.listen(process.env.PORT || 8080)
+app.listen(process.env.PORT || 8080);
+
+
